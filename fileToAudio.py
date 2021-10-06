cf3765ca5e0b91c7307e432ebdd5dd537d6557c6
Manisha Kumari Sah <60925015+manisha-4@users.noreply.github.com>
2021-10-06 20:22:51 +0530

    Create fileToAudio.py

---
+++ fileToAudio.py
@@ -0,0 +1,26 @@
+from gtts import gTTS
+from pdfminer.high_level import extract_text
+import gradio as gr
+
+def pdf_to_text(file_obj):
+    text = extract_text(file_obj.name)
+    myobj = gTTS(text=text, lang='en', slow=False) 
+    myobj.save("test.wav") 
+    return 'test.wav'
+
+examples = [
+    [os.path.abspath("short-pdf.pdf")],
+    [os.path.abspath("long-pdf.pdf")]
+]
+
+iface = gr.Interface(fn = pdf_to_text, 
+                     inputs = 'file', 
+                     outputs = 'audio', 
+                     title = 'PDF into speech Application',
+                     description = 'Simple application in python to try the Gradio package components',
+                     article = 
+                        '''<div>
+                            <p> All you need to do is to upload the pdf file and hit submit, then wait for compiling. After that click on Play/Pause for listening to the audio. The audio is saved in a wav format.</p>
+                        </div>'''
+                    )
+iface.launch()
